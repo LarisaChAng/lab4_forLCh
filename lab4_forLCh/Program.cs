a585f7480863036dee9cf4e43c2e2936f9c3d552
LarisaChAng <LChS81@mail.ru>
2022-10-21 17:13:37 +0800

    Добавьте файлы проекта.

---
+++ lab4_forLCh/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4_forLCh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            double m = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                s += 2 * i - 1;
+                m = Math.Pow(s, 2);
+
+                Console.WriteLine(m);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
